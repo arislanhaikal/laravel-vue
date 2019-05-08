28f4e646d7b76b8802c5087b2f66941d6756fa51
Arislan Haikal <arislanhaikal@gmail.com>
2019-05-08 19:08:00 +0700

    minify assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,6 +44,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('contact') }}">{{ __('Contact') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin') }}">{{ __('Admin') }}</a>
+                        </li>
                     </ul>
                 </div>
             </div>
